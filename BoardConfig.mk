14c69f60d585b3ac25ca054f111e6f540c77e219
code-acezero <codeacezero@gmail.com>
2025-12-22 14:56:14 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 # Kernel - Prebuilt
 # FIX: Use relative path + .img extension to match standard Android expectations.
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
