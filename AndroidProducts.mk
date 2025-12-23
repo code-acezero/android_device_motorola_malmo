bfbbe35528572dfa71b11d1eb4f994a95ac98c07
code-acezero <codeacezero@gmail.com>
2025-12-23 20:48:07 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,13 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    device/motorola/malmo/twrp_malmo.mk
+    $(LOCAL_DIR)/twrp_malmo.mk
 
 COMMON_LUNCH_CHOICES := \
+    twrp_malmo-eng \
     twrp_malmo-user \
-    twrp_malmo-userdebug \
-    twrp_malmo-eng
+    twrp_malmo-userdebug
