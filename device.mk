e54a95d3939560f21fa86c7dc7909d8a8397ecef
code-acezero <codeacezero@gmail.com>
2025-12-23 20:47:26 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,23 +7,22 @@
 LOCAL_PATH := device/motorola/malmo
 
 # ---------------------------------------------------------
-# 1. CRITICAL: COPY FILES TO RAMDISK (The Missing Fix)
+# 1. COPY FILES (Cleaned Up)
 # ---------------------------------------------------------
-# This section forces the Kernel and Init Scripts into the image.
-# Without this, you get an empty generic recovery.
+# Note: Kernel/DTB are handled by BoardConfig.mk now.
+# We only copy the config scripts here.
 PRODUCT_COPY_FILES += \
-   
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
     $(LOCAL_PATH)/recovery/root/servicemanager.recovery.rc:recovery/root/servicemanager.recovery.rc
 
-# Copy fstab if it exists in the standard location
-# (If your fstab is in a different folder, adjust this path)
+# Copy Fstab and Flags
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:recovery/root/system/etc/recovery.fstab
+    $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:recovery/root/system/etc/recovery.fstab \
+    $(LOCAL_PATH)/recovery/root/system/etc/twrp.flags:recovery/root/system/etc/twrp.flags
 
 # ---------------------------------------------------------
-# 2. A/B PARTITION CONFIGURATION
+# 2. A/B CONFIGURATION
 # ---------------------------------------------------------
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -33,17 +31,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # ---------------------------------------------------------
-# 3. BOOT CONTROL HAL (Safe Mode)
+# 3. BOOT CONTROL
 # ---------------------------------------------------------
-# Using the generic Android Boot HAL prevents "Module not found" errors
-# on minimal manifests.
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl.recovery
-
-# Standard Update Tools
-PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl.recovery \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
@@ -52,13 +45,12 @@ PRODUCT_PACKAGES += \
     bootctrl.malmo
 
 # ---------------------------------------------------------
-# 4. QCOM TOOLS & LIBRARIES
+# 4. LIBRARIES
 # ---------------------------------------------------------
 PRODUCT_PACKAGES += \
     libion \
     libxml2
 
-# Allow TWRP to see the full screen
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.twrp.vendor_boot=true \
     ro.boot.dynamic_partitions=true
