4fc2583bbecefffcc68fd6c0dd82daca9333731d
code-acezero <codeacezero@gmail.com>
2025-12-23 21:49:07 +0600

    Update twrp_malmo.mk

--- twrp_malmo.mk
+++ twrp_malmo.mk
@@ -4,21 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# 1. Inherit Device Config
+# 1. Inherit Device Config (Must come first)
 $(call inherit-product, device/motorola/malmo/device.mk)
 
 # 2. Inherit Core Android
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# 3. Inherit TWRP Config (The "Library")
+# 3. Inherit TWRP Config
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# 4. Product Definition (RENAMED TO TWRP)
+# 4. Product Definition
 PRODUCT_NAME := twrp_malmo
 PRODUCT_DEVICE := malmo
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g85 5G
+PRODUCT_MODEL := Moto G85 5G
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
